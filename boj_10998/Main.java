1ded463f5a4f1e3ceab4b04a0c5cf578e2994725
jay-so <52352476+jay-so@users.noreply.github.com>
2020-12-30 16:57:41 +0900

    백준 온라인 10998번

---
+++ boj_10998/Main.java
@@ -0,0 +1,26 @@
+package boj_10998;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if((0 < a) && (a >= 10)) {
+			System.out.println("a에 0이상 10미만의 수를 입력해주세요");
+			return;
+		}
+		
+		if((0<b)&&(b >= 10)) {
+			System.out.println("b에 0이상 10미만의 수를 입력해 주세요");
+			return;
+		}
+		
+		System.out.println(a*b);
+	}
+
+}
